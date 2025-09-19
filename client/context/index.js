1d5239c7394d2cdc4895abc32d9a6c8dd8d4791a
Tanmayasahoo3536 <tanmayasahoo3536@gmail.com>
2025-09-19 18:50:22 +0530

    is baar kam karja plz

--- client/context/index.js
+++
@@ -1,4 +0,0 @@
-import { AuthContext } from "./AuthContext.jsx";
-import {ChatContext} from './ChatContext.jsx'
-
-export {AuthContext,ChatContext};
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react'
 import assets from '../assets/assets'
 import { useNavigate } from 'react-router-dom';
-// import { AuthContext } from '../../context/AuthContext.jsx';
-// import { ChatContext } from '../../context/ChatContext.jsx';
-import {AuthContext,ChatContext} from '../../context/index.js'
+import { AuthContext } from '../../context/AuthContext.jsx';
+import { ChatContext } from '../../context/chatContext.jsx';
+
 
 const Sidebar = () => {
 

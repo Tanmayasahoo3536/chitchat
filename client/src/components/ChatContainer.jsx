409ef1c131abdaf197bc72477254a745c86ad05a
Tanmayasahoo3536 <tanmayasahoo3536@gmail.com>
2025-09-19 19:08:04 +0530

    cache memory resolved

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -3,7 +3,7 @@ import assets, { messagesDummyData } from '../assets/assets'
 import { formatMessageTime } from '../lib/utils'
 import { AuthContext } from '../../context/AuthContext'
 import toast from 'react-hot-toast'
-import { ChatContext } from '../../context/chatContext'
+import { ChatContext } from '../../context/ChatContext'
 
 const ChatContainer = () => {
 
